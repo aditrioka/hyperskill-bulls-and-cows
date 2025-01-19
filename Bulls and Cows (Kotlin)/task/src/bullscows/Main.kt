eb55c98e21e31c63bd921feb0f8c567be8775c49
Adi Trioka <aditrioka@gmail.com>
2025-01-19 22:03:37 +0700

    finish third stage

--- Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
+++ Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
@@ -2,9 +2,11 @@ package bullscows
 
 const val WELCOME_MESSAGE = "The secret code is prepared: ****."
 const val SECRET_CODE = 9305
+const val ERROR_MESSAGE = "Error: can't generate a secret number with a length of 11 because there aren't enough unique digits."
+const val MAX_LENGTH = 10
 
 fun main() {
-    grade()
+    generatePseudoRandomNumber()
 }
 
 fun printMessage() {
@@ -23,8 +25,8 @@ fun printMessage() {
 }
 
 fun grade() {
-    val input = readln().toCharArray().map { it.code }
-    val secretCode = SECRET_CODE.toString().toCharArray().map { it.code }
+    val input = readln().toCharArray().map { it.toString().toInt() }
+    val secretCode = SECRET_CODE.toString().toCharArray().map { it.toString().toInt() }
 
     var numOfBulls = 0
     var numOfCows = 0
@@ -45,4 +47,33 @@ fun grade() {
     numOfCows -= numOfBulls
 
     println("Grade: $numOfBulls bull(s) and $numOfCows cow(s). The secret code is $SECRET_CODE.")
+}
+
+fun generatePseudoRandomNumber() {
+    val length = readln().toInt()
+
+    if (length > MAX_LENGTH) {
+        println(ERROR_MESSAGE)
+    } else {
+        var pseudoRandomNumber: String
+
+        do {
+            pseudoRandomNumber = ""
+            val numberCount = BooleanArray(10)
+
+            val systemNanoTime = System.nanoTime().toString().reversed()
+            val charArray = systemNanoTime.toCharArray().map { it.digitToInt() }
+
+            charArray.forEach { digit ->
+                if (!numberCount[digit]) {
+                    numberCount[digit] = true
+                    pseudoRandomNumber += digit
+                }
+            }
+
+            val isNumberValid = pseudoRandomNumber.length >= length
+        } while(!isNumberValid)
+
+        println(pseudoRandomNumber.substring(0,  length))
+    }
 }
