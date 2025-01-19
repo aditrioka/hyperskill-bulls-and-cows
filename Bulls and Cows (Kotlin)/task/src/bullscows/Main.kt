a1174189ec15937ee14fa9fbd430eab6e907cfe8
Adi Trioka <aditrioka@gmail.com>
2025-01-19 19:57:40 +0700

    finish first stage

--- Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
+++ Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
@@ -1,5 +1,18 @@
 package bullscows
 
+const val WELCOME_MESSAGE = "The secret code is prepared: ****."
+
 fun main() {
-    println("Hello World!")
+    println(WELCOME_MESSAGE)
+
+    println("""
+        Turn 1. Answer:
+        1234
+        Grade: None.
+
+        Turn 2. Answer:
+        9876
+        Grade: 4 bulls.
+        Congrats! The secret code is 9876.
+    """.trimIndent())
 }
