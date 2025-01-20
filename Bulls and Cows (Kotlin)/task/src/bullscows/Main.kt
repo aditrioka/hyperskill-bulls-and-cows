561669e5e714e4ee1f8a55282cad0c99eea01857
Adi Trioka <aditrioka@gmail.com>
2025-01-20 20:55:43 +0700

    finish sixth stage

--- Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
+++ Bulls and Cows (Kotlin)/task/src/bullscows/Main.kt
@@ -1,5 +1,7 @@
 package bullscows
 
+import kotlin.random.Random
+
 fun main() {
     val bullsAndCows = BullsAndCows()
     bullsAndCows.play()
@@ -8,13 +10,21 @@ fun main() {
 class BullsAndCows {
 
     private var turn = 1
-    private var secretCode = mutableListOf<Int>()
+    private var secretCode = mutableListOf<Char>()
     private var isGameOver = false
+    private var lengthOfSecretCode = 0
+    private var numOfPossibleSymbols = 0
+    private var isInputValid = false
+
+    private val numberRange = '0'..'9'
+    private val alphabetRange = 'a'..'z'
+    private val characterList = numberRange + alphabetRange
+
+    private val characterFlags = mutableSetOf<Char>()
 
     fun play() {
-        println(ENTER_LENGTH_PROMPT)
-        val length = readln().toInt()
-        generateNumber(length)
+        promptInput()
+        generateCode()
 
         println(START_GAME_MESSAGE)
         do {
@@ -27,8 +37,24 @@ class BullsAndCows {
         println(WIN_MESSAGE)
     }
 
+    private fun promptInput() {
+        do {
+            println(INPUT_LENGTH_PROMPT)
+            lengthOfSecretCode = readln().toInt()
+
+            println(INPUT_NUM_SYMBOLS)
+            numOfPossibleSymbols = readln().toInt()
+
+            isInputValid = lengthOfSecretCode in 1..MAX_LENGTH &&
+                    numOfPossibleSymbols in 2..MAX_LENGTH &&
+                    lengthOfSecretCode <= numOfPossibleSymbols
+
+            if (!isInputValid) println(ERROR_MESSAGE)
+        } while(!isInputValid)
+    }
+
     private fun grade(guess: String) {
-        val input = guess.toCharArray().map { it.toString().toInt() }
+        val input = guess.toCharArray()
 
         var numOfBulls = 0
         var numOfCows = 0
@@ -55,46 +81,57 @@ class BullsAndCows {
         if (numOfBulls == secretCode.size) isGameOver = true
     }
 
-    private fun generateNumber(length: Int) {
-        if (length > MAX_LENGTH) {
-            println(ERROR_MESSAGE)
-        } else {
-            var pseudoRandomNumber: MutableList<Int>
+    private fun generateCode() {
+        var isConditionMet: Boolean
+        do {
+            val index = Random.nextInt(0, numOfPossibleSymbols)
+            val selectedChar = characterList[index]
 
-            do {
-                pseudoRandomNumber = emptyList<Int>().toMutableList()
-                val numberCount = BooleanArray(10)
+            if (!characterFlags.contains(selectedChar)) characterFlags.add(selectedChar)
 
-                val systemNanoTime = System.nanoTime().toString().reversed()
-                val charArray = systemNanoTime.toCharArray().map { it.digitToInt() }
+            isConditionMet = characterFlags.size == numOfPossibleSymbols
+        } while(!isConditionMet)
 
-                charArray.forEach { digit ->
-                    if (!numberCount[digit]) {
-                        numberCount[digit] = true
-                        pseudoRandomNumber += digit
-                    }
+        secretCode.addAll(characterFlags.take(lengthOfSecretCode))
+        val asterisks = "*".repeat(lengthOfSecretCode)
+        val range = getTextOfRange()
+
+        println(SECRET_PREPARED_MESSAGE.format(asterisks, range))
+    }
+
+    private fun getTextOfRange(): String {
+        val remainingTextOfRange = when {
+            numOfPossibleSymbols == 2 -> "1"
+            numOfPossibleSymbols <= DIGIT_LENGTH -> "${characterList[numOfPossibleSymbols - 1]}"
+
+            else -> {
+                if (numOfPossibleSymbols - DIGIT_LENGTH == 1) {
+                    "9, a-a"
+                } else {
+                    "9, a-${characterList[numOfPossibleSymbols - 1]}"
                 }
-
-                val isNumberValid = pseudoRandomNumber.size >= length
-            } while(!isNumberValid)
-
-            secretCode.addAll(pseudoRandomNumber.take(length))
+            }
         }
+
+        return "(0-%s).".format(remainingTextOfRange)
     }
 
     companion object {
-        const val WELCOME_MESSAGE = "The secret code is prepared: ****."
         const val ERROR_MESSAGE = "Error: can't generate a secret number with a length of 11 because there aren't enough unique digits."
-        const val MAX_LENGTH = 10
-        const val ENTER_LENGTH_PROMPT = "Please, enter the secret code's length:"
+        const val MAX_LENGTH = 36
+        const val DIGIT_LENGTH = 10
+
+        const val INPUT_LENGTH_PROMPT = "Input the length of the secret code:"
+        const val INPUT_NUM_SYMBOLS = "Input the number of possible symbols in the code:"
         const val START_GAME_MESSAGE = "Okay, let's start a game!"
+
         const val SINGULAR_BULL = "bull"
         const val SINGULAR_COW = "cow"
         const val PLURAL_BULL = "bulls"
         const val PLURAL_COW = "cows"
+
         const val WIN_MESSAGE = "Congratulations! You guessed the secret code."
-        const val GRADE_TEXT = "Grade: "
-        const val AND_TEXT = "and"
         const val TURN_TEXT = "Turn %d:"
+        const val SECRET_PREPARED_MESSAGE = "The secret is prepared: %1\$s %2\$s"
     }
 }
